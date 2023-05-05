f2fb6529afff53bad132ae22a62159dd72e3c5dd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-04 21:42:30 -0300

    Creating project

---
+++ src/main/java/br/com/fiap/study/thymeleaf/StudyThymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.study.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/study/thymeleaf/StudyThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.study.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
