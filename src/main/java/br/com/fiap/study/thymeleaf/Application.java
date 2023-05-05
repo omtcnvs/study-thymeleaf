4be5ba846c51f74af5d216445a9a505ecd4339f8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-04 22:47:01 -0300

    Implementing controller with index.html

--- src/main/java/br/com/fiap/study/thymeleaf/Application.java
+++ src/main/java/br/com/fiap/study/thymeleaf/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyThymeleafApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyThymeleafApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/br/com/fiap/study/thymeleaf/controllers/IndexController.java
+++ src/main/java/br/com/fiap/study/thymeleaf/controllers/IndexController.java
@@ -1,5 +1,17 @@
 package br.com.fiap.study.thymeleaf.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class IndexController {
 
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("message", "Hello World!");
+		return "index.html";
+	}
+	
 }
